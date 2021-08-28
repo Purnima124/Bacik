2f60f5d0ae59a3304dc4c21016026bf50dea8d05
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2021-08-28 11:23:12 +0530

    Add files via upload

---
+++ ppramition.py
@@ -0,0 +1,14 @@
+# hospital=input("take premition")
+# if hospital=="dissko se":
+#     print("permition mil gai")
+# else:
+#     print("permition nahi mila")
+# hospital jana hai
+hospital=input("I will go hospital")
+if hospital=="dissko ":
+    print("permition mil gai")
+elif hospital=="time member":
+    print("pramition mil gaya")
+else:
+    print("permition nahi mila")
+
---
+++ variable.py
@@ -0,0 +1,31 @@
+# a="purnima"
+# b="19"
+# print(a+b)
+
+# print(a+"b")
+
+# # conversion
+# var56="20"
+# var57=20
+# print(type(var57))
+
+# var23=3.0
+# var24=int(var23)
+# print(type(var24))
+
+# a="@@*************@@"
+# print(a,"@@@@@@@@")
+# print(a,"@@@@@@@")
+
+# a="purnima"
+# b=1997
+# b=str(b)
+# print(a,b)
+
+
+
+
+
+
+
+
